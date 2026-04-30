37a96485e1775908b0b011089eacc38a8594c8b7
Natalia Giraldo <ngiraldo@tsomobile.com>
2026-04-30 07:55:19 -0500

    log

--- playwright.service.config.js
+++ playwright.service.config.js
@@ -1,13 +1,19 @@
 const { defineConfig } = require('@playwright/test');
 const { createAzurePlaywrightConfig, ServiceOS } = require('@azure/playwright');
+const { DefaultAzureCredential, ClientSecretCredential } = require('@azure/identity');
 const config = require('./playwright.config');
+
+const credential = process.env.AZURE_CLIENT_ID
+  ? new ClientSecretCredential(process.env.AZURE_TENANT_ID, process.env.AZURE_CLIENT_ID, process.env.AZURE_CLIENT_SECRET)
+  : new DefaultAzureCredential();
 
 module.exports = defineConfig(
   config,
-  createAzurePlaywrightConfig({
+  createAzurePlaywrightConfig(config, {
     exposeNetwork: '<loopback>',
     connectTimeout: 3 * 60 * 1000,
     os: ServiceOS.LINUX,
+    credential,
   }),
   {
     reporter: [
